1b85186a82e22eda4613a74e05cc941f6ed6ee32
Valentin Brosseau <c4software@gmail.com>
2018-12-10 18:12:34 +0100

    Update

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def print_photo(printer, height, width):
         return jsonify({"status": 1, "reason": 'Please provide a valid jpeg file (multipart/form-data).'})
 
 
-
 @app.route("/status/<printer>")
 def status(printer):
     return jsonify(get_printer_status(printer))
