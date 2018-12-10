0d9f9a7bb6b3bfd641e993f02164464f0d4f5953
Valentin Brosseau <c4software@gmail.com>
2018-12-10 15:27:57 +0100

    Add status

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ app = Flask(__name__)
 
 @app.route('/print/photo/<printer>/<height>/<width>', methods=["POST"])
 def print_photo(printer, height, width):
-    print(request.files)
     if 'file' in request.files:
         f = request.files['file']
         return print_image(printer, f.stream, int(height), int(width))
     else:
         return "{'error': 'Please provide a valid jpeg file (multipart/form-data).'}"
 
+
+@app.route("/status")
+def status():
+    return "1"
