d30db4d7b3fde5d74294300dc6256c65027b8382
Valentin Brosseau <c4software@gmail.com>
2019-11-20 22:40:31 +0100

    Adjust settings

--- helper/dslr.py
+++ helper/dslr.py
@@ -1,7 +1,16 @@
 import subprocess
+from flask import send_file
+
 
 def take_shot():
     try:
-        return subprocess.check_output("gphoto2 --stdout", shell=True)
+        subprocess.check_output("gphoto2 --force-overwrite --quiet --capture-image-and-download --filename /tmp/photobooth.jpg", shell=True)
+        return send_file("/tmp/photobooth.jpg")
+    except:
+        return ""
+
+def get_summary():
+    try:
+        return subprocess.check_output("gphoto2 --summary", shell=True)
     except:
         return ""
